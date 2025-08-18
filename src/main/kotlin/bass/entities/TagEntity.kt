02c89d1277b9424e29016f7927c4f1577c0802fa
arekiu <alexis.aguilera@deliveryhero.com>
2025-08-18 20:39:02 +0200

    feat(Tag): add relation to Meal

--- src/main/kotlin/bass/entities/TagEntity.kt
+++ src/main/kotlin/bass/entities/TagEntity.kt
@@ -2,9 +2,11 @@ package bass.entities
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.ManyToMany
 import jakarta.persistence.Table
 import jakarta.persistence.UniqueConstraint
 
@@ -16,6 +18,8 @@ import jakarta.persistence.UniqueConstraint
 class TagEntity(
     @Column(nullable = false)
     val name: String,
+    @ManyToMany(mappedBy = "tags", fetch = FetchType.LAZY)
+    val meals: MutableSet<MealEntity> = mutableSetOf(),
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
