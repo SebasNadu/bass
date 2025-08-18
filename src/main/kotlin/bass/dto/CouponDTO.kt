eb0919640b7a0e44b5985eae6d8b2d613a6eb09d
arekiu <alexis.aguilera@deliveryhero.com>
2025-08-18 11:49:11 +0200

    refactor(CouponDTO): use Instant instead LocalDateTime

--- src/main/kotlin/bass/dto/CouponDTO.kt
+++ src/main/kotlin/bass/dto/CouponDTO.kt
@@ -1,11 +1,11 @@
 package bass.dto
 
-import java.time.LocalDateTime
+import java.time.Instant
 
 data class CouponDTO(
     val name: String,
     val memberId: Long,
     val discountRate: String? = null,
-    val expiresAt: LocalDateTime? = null,
+    val expiresAt: Instant? = null,
     var id: Long = 0L,
 )
