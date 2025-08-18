e1bb0996dfb96bbfc2b173f1019a952c042181bb
Bojana Petroska <bojana.petroska@yahoo.com>
2025-08-18 14:30:18 +0200

    refactor(data): rename file name

--- src/main/resources/db/changelog/changes/20250816-1600-create-tag-table.sql
+++ src/main/resources/db/changelog/changes/20250816-1600-create-tag-table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE tag
 (
     id            BIGSERIAL PRIMARY KEY,
-    name          VARCHAR(50) NOT NULL
+    name          VARCHAR(50) NOT NULL,
+    created_at    TIMESTAMP   NOT NULL,
+    updated_at    TIMESTAMP   NOT NULL
 );
