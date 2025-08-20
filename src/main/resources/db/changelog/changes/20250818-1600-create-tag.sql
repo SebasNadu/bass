601e01bb7fd494d1a25ec1b5c4fd7b5fe705a0ad
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-08-21 00:13:07 +0200

    feat: add achievement and coupon tables with timestamps

--- src/main/resources/db/changelog/changes/20250818-1600-create-tag.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tag
-(
-    id            BIGSERIAL PRIMARY KEY,
-    name          VARCHAR(50) NOT NULL,
-    created_at    TIMESTAMP   NOT NULL,
-    updated_at    TIMESTAMP   NOT NULL
-);
