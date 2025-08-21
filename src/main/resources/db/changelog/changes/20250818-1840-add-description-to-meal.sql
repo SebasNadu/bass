4e8d320fb4260987a46a7f6c0884f09bcc9ddf00
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-08-21 19:35:05 +0200

    feat(liquibase): clean changelog before deploying with the production db

--- src/main/resources/db/changelog/changes/20250818-1840-add-description-to-meal.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE meal ADD COLUMN description VARCHAR(255);
--- src/main/resources/db/changelog/changes/20250818-1850-create-join-tag-meal.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tag_meal
-(
-    meal_id BIGINT NOT NULL,
-    tag_id  BIGINT NOT NULL,
-    CONSTRAINT fk_tag_meal_on_meal FOREIGN KEY (meal_id) REFERENCES meal (id) ON DELETE CASCADE,
-    CONSTRAINT fk_tag_meal_on_tag FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE,
-    PRIMARY KEY (meal_id, tag_id)
-);
