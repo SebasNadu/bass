1d92745145c27738efacad4832aaf5a5c93f0821
Sanghun <po4865@gmail.com>
2025-08-21 15:57:30 +0200

    fix(sql): add default to timestamp properties

--- src/main/resources/db/changelog/changes/20250820-1600-create-day.sql
+++ src/main/resources/db/changelog/changes/20250820-1600-create-day.sql
@@ -3,6 +3,6 @@ CREATE TABLE day_of_week
      id           BIGSERIAL PRIMARY KEY,
      day_name     VARCHAR(20) NOT NULL,
      member_id    BIGINT      NOT NULL REFERENCES member (id),
-     created_at   TIMESTAMP   NOT NULL,
-     updated_at   TIMESTAMP   NOT NULL
+     created_at   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     updated_at   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
