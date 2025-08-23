b4941ee50e1563ed0f9346ba5c3138e94ac59baf
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-08-23 15:08:04 +0200

    feat(controller): add natural search endpoint to MealController

--- src/main/kotlin/bass/repositories/MealRepository.kt
+++ src/main/kotlin/bass/repositories/MealRepository.kt
@@ -2,9 +2,34 @@ package bass.repositories
 
 import bass.entities.MealEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface MealRepository : JpaRepository<MealEntity, Long> {
     fun existsByName(name: String): Boolean
 
     fun findByTagsName(tagName: String): List<MealEntity>
+
+    @Query(
+        """
+        select distinct m from MealEntity m
+        join m.tags t
+        where lower(t.name) in :tagNames
+        """
+    )
+    fun findAnyTag(@Param("tagNames") tagNames: Collection<String>): List<MealEntity>
+
+    @Query(
+        """
+        select m from MealEntity m
+        join m.tags t
+        where lower(t.name) in :tagNames
+        group by m
+        having count(distinct lower(t.name)) = :requiredCount
+    """
+    )
+    fun findAllTags(
+        @Param("tagNames") tagNames: Collection<String>,
+        @Param("requiredCount") requiredCount: Long
+    ): List<MealEntity>
 }
