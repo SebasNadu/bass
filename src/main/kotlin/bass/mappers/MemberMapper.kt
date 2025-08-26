32c9933e77aed8c2d3e194b6b9e9a7d1f4e63cd7
Sanghun <po4865@gmail.com>
2025-08-26 15:41:58 +0200

    feat(member): apply 'freedomDays' on 'MemberMapper'

--- src/main/kotlin/bass/mappers/MemberMapper.kt
+++ src/main/kotlin/bass/mappers/MemberMapper.kt
@@ -2,11 +2,16 @@ package bass.mappers
 
 import bass.dto.member.MemberLoginDTO
 import bass.dto.member.MemberRegisterDTO
+import bass.entities.DayEntity
 import bass.entities.MemberEntity
 import bass.entities.TagEntity
 import bass.model.Member
 
-fun MemberEntity.toDTO() = Member(name, email, password, role, testimonial, id)
+fun MemberEntity.toDTO(): Member {
+    val freedomDays = days.map { it.dayName.name }.toSet()
+    val tags = tags.map { it.toDTO() }.toSet()
+    return Member(name, email, password, role, testimonial, freedomDays, tags, id)
+}
 
 fun Member.toLoginDTO() = MemberLoginDTO(id)
 
