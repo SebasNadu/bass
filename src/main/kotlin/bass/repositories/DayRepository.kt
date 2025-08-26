6d32e04c5d45b9ec30e2f36c98540da65cee786f
Sanghun <po4865@gmail.com>
2025-08-26 16:45:23 +0200

    chore: remove unnecessary method

--- src/main/kotlin/bass/repositories/DayRepository.kt
+++ src/main/kotlin/bass/repositories/DayRepository.kt
@@ -4,7 +4,5 @@ import bass.entities.DayEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface DayRepository : JpaRepository<DayEntity, Long> {
-    fun findAllByMemberId(id: Long): List<DayEntity>
-
     fun findByMemberId(memberId: Long): List<DayEntity>
 }
