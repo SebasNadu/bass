f6e9bacbd1fa7c0798c856523e74a1541be1a9a9
Bojana Petroska <bojana.petroska@yahoo.com>
2025-08-27 17:19:51 +0200

    feat(all_orders_for_member): add mealName & imageUrl in OrderItemDTO

--- src/main/kotlin/bass/mappers/OrderItemMapper.kt
+++ src/main/kotlin/bass/mappers/OrderItemMapper.kt
@@ -3,10 +3,11 @@ package bass.mappers
 import bass.dto.order.OrderItemDTO
 import bass.entities.OrderItemEntity
 
-fun OrderItemEntity.toDTO() =
-    OrderItemDTO(
-        mealId = meal.id,
-        quantity = quantity,
-        price = price,
-        id = id,
-    )
+fun OrderItemEntity.toDTO() = OrderItemDTO(
+    mealId = meal.id,
+    quantity = quantity,
+    price = price,
+    mealName = meal.name,
+    imageUrl = meal.imageUrl,
+    id = id,
+)
